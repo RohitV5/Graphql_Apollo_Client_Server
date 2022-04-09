7d15ae8e3b2a183b1aa545c4a5c0a9cf06024471
Rohit Verma <verma.rohit.in@gmail.com>
2022-04-09 16:48:21 +0530

    feature: signin and get jwt token

--- resolvers.js
+++ resolvers.js
@@ -1,8 +1,10 @@
 import pc from "@prisma/client";
 import { ApolloError, AuthenticationError } from "apollo-server";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const prisma = new pc.PrismaClient();
+
 
 const resolvers = {
   Query: {},
@@ -21,6 +23,15 @@ const resolvers = {
       });
       return newUser;
     },
+    signinUser: async (_,{userSignin}) =>{
+        const user = await prisma.user.findUnique({where:{email:userSignin.email}})
+        if(!user) throw new AuthenticationError("User does not exist with that email");
+        const doMatch = await bcrypt.compare(userSignin.password,user.password);
+        if(!doMatch) throw new AuthenticationError("Password is incorrect");
+        const token = jwt.sign({userId:user.id},process.env.JWT_SECRET)
+        return {token}
+
+    }
   },
 };
 
