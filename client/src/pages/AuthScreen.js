5f2a86834d781edab42ea858295c009c51b84bf4
Rohit Verma <verma.rohit.in@gmail.com>
2022-04-10 19:41:28 +0530

    feature: signin-form

--- client/src/pages/AuthScreen.js
+++ client/src/pages/AuthScreen.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import { Box, Stack, Typography, Button, TextField } from "@mui/material";
+import React, { useState, useRef } from "react";
+import { Box, Stack, Typography, Button, TextField, Card } from "@mui/material";
 
 function AuthScreen() {
+  const [showLogin, setShowLogin] = useState(true);
   const [formData, setFormData] = useState({});
-
+  const authForm = useRef(null);
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -17,6 +18,7 @@ function AuthScreen() {
   };
   return (
     <Box
+      ref={authForm}
       component="form"
       onSubmit={handleSubmit}
       display="flex"
@@ -24,35 +26,56 @@ function AuthScreen() {
       alignItems="center"
       height="80vh"
     >
-      <Stack direction="column" spacing={2} sx={{ width: "400px" }}>
-        <Typography variant="h5">Please Signup</Typography>
-        <TextField
-          label="First Name"
-          variant="standard"
-          name="firstName"
-          onChange={handleChange}
-        />
-        <TextField
-          label="Last Name"
-          variant="standard"
-          name="lastname"
-          onChange={handleChange}
-        />
-        <TextField
-          label="Email"
-          variant="standard"
-          name="email"
-          onChange={handleChange}
-        />
-        <TextField
-          label="Password"
-          variant="standard"
-          name="password"
-          onChange={handleChange}
-          type="password"
-        />
-        <Button variant="outlined">Sign up</Button>
-      </Stack>
+      <Card sx={{ padding: "10px" }} variant="outlined">
+        <Stack direction="column" spacing={2} sx={{ width: "400px" }}>
+          <Typography variant="h5">
+            Please {showLogin ? "Signin" : "Signup"}
+          </Typography>
+          {!showLogin && (
+            <>
+              <TextField
+                label="First Name"
+                variant="standard"
+                name="firstName"
+                onChange={handleChange}
+              />
+              <TextField
+                label="Last Name"
+                variant="standard"
+                name="lastname"
+                onChange={handleChange}
+              />
+            </>
+          )}
+          <TextField
+            label="Email"
+            variant="standard"
+            name="email"
+            onChange={handleChange}
+          />
+
+          <TextField
+            label="Password"
+            variant="standard"
+            name="password"
+            onChange={handleChange}
+            type="password"
+          />
+          <Typography
+            variant="subtitle1"
+            onClick={() => {
+              setShowLogin(!showLogin);
+              setFormData({})
+              authForm.current.reset();
+            }}
+          >
+            {showLogin ? "Signup?" : "Login"}
+          </Typography>
+          <Button variant="contained" type="submit">
+            {showLogin ? "Signin" : "Signup"}
+          </Button>
+        </Stack>
+      </Card>
     </Box>
   );
 }
